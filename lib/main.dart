a6c78f083b8691e78ffb8019d9778e01684e8c49
Tanay <tanaygupta811@gmail.com>
2024-09-23 22:04:40 +0530

    patch 1.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,164 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:record/record.dart';
+import 'package:vosk_flutter/vosk_flutter.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: VoskFlutterDemo(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class VoskFlutterDemo extends StatefulWidget {
+  const VoskFlutterDemo({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<VoskFlutterDemo> createState() => _VoskFlutterDemoState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _VoskFlutterDemoState extends State<VoskFlutterDemo> {
+  static const _textStyle = TextStyle(fontSize: 30, color: Colors.black);
+  static const _modelName = 'vosk-model-small-en-us-0.15';
+  static const _sampleRate = 16000;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  final _vosk = VoskFlutterPlugin.instance();
+  final _modelLoader = ModelLoader();
+  final _recorder = AudioRecorder();
+
+  String? _fileRecognitionResult;
+  String? _error;
+  Model? _model;
+  Recognizer? _recognizer;
+  SpeechService? _speechService;
+
+  bool _recognitionStarted = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _modelLoader
+        .loadFromAssets('assets/models/vosk-model-small-en-us-0.15.zip')
+        .then((modelPath) => _vosk.createModel(modelPath))
+        .then((model) => setState(() => _model = model))
+        .then((_) => _vosk.createRecognizer(
+            model: _model!, sampleRate: _sampleRate)) // create recognizer
+        .then((value) => _recognizer = value)
+        .then((recognizer) {
+      if (Platform.isAndroid) {
+        _vosk
+            .initSpeechService(_recognizer!) // init speech service
+            .then((speechService) =>
+                setState(() => _speechService = speechService))
+            .catchError((e) => setState(() => _error = e.toString()));
+      }
+    }).catchError((e) {
+      setState(() => _error = e.toString());
+      return null;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    if (_error != null) {
+      return Scaffold(
+          body: Center(child: Text("Error: $_error", style: _textStyle)));
+    } else if (_model == null) {
+      return const Scaffold(
+          body: Center(child: Text("Loading model...", style: _textStyle)));
+    } else if (Platform.isAndroid && _speechService == null) {
+      return const Scaffold(
+        body: Center(
+          child: Text("Initializing speech service...", style: _textStyle),
+        ),
+      );
+    } else {
+      return Platform.isAndroid ? _androidExample() : _commonExample();
+    }
+  }
+
+  Widget _androidExample() {
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          children: [
+            ElevatedButton(
+                onPressed: () async {
+                  if (_recognitionStarted) {
+                    await _speechService!.stop();
+                  } else {
+                    await _speechService!.start();
+                  }
+                  setState(() => _recognitionStarted = !_recognitionStarted);
+                },
+                child: Text(_recognitionStarted
+                    ? "Stop recognition"
+                    : "Start recognition")),
+            StreamBuilder(
+                stream: _speechService!.onPartial(),
+                builder: (context, snapshot) => Text(
+                    "Partial result: ${snapshot.data.toString()}",
+                    style: _textStyle)),
+            StreamBuilder(
+                stream: _speechService!.onResult(),
+                builder: (context, snapshot) => Text(
+                    "Result: ${snapshot.data.toString()}",
+                    style: _textStyle)),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget _commonExample() {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () async {
+                  if (_recognitionStarted) {
+                    await _stopRecording();
+                  } else {
+                    await _recordAudio();
+                  }
+                  setState(() => _recognitionStarted = !_recognitionStarted);
+                },
+                child: Text(
+                    _recognitionStarted ? "Stop recording" : "Record audio")),
+            Text("Final recognition result: $_fileRecognitionResult",
+                style: _textStyle),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> _recordAudio() async {
+    try {
+      await _recorder.start(const RecordConfig(), path: 'aFullPath/myFile.m4a');
+    } catch (e) {}
+  }
+
+  Future<void> _stopRecording() async {
+    try {
+      final filePath = await _recorder.stop();
+      if (filePath != null) {
+        final bytes = File(filePath).readAsBytesSync();
+        _recognizer!.acceptWaveformBytes(bytes);
+        _fileRecognitionResult = await _recognizer!.getFinalResult();
+      }
+    } catch (e) {}
   }
 }
